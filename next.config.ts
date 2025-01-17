72a10c7bd3e04dadafd1acd5bbc27ed5efae46ca
m02uku <m02ukuzrzr@gmail.com>
2025-01-17 13:20:06 +0900

    refactor: :fire:

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    distDir: ".next/out",
 };
 
 export default nextConfig;
